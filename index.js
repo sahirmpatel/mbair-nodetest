fe4df084d0761dc12bd4a5e2405f2d8fd17c7aa6
sahirmpatel <sahirmpatel@gmail.com>
2022-09-30 16:56:13 +0530

    intial commit 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 app.get("/", (res, req) => {
-  req.send("Hello World");
+  req.send("Hello");
 });
 
 app.listen(PORT, () => console.log("listening on port " + PORT));
