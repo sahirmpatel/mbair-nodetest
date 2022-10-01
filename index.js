f081aa78ffd6ce590f40416e5485bccbf1cf10a8
sahirmpatel <sahirmpatel@gmail.com>
2022-10-01 11:53:11 +0530

    docker setup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.get("/", (res, req) => {
-  req.send("Hello");
+  req.json({ message: "Docker is live 🐳" });
 });
 
 app.listen(PORT, () => console.log("listening on port " + PORT));
